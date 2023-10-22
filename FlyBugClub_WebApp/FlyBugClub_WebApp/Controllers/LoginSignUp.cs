fbcf1544c9c1f6d4d62702688f8cad2b1ebf6382
Danny <nqd230899@gmail.com>
2023-10-22 23:39:57 +0700

    Update otp page

---
+++ FlyBugClub_WebApp/FlyBugClub_WebApp/Controllers/LoginSignUp.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlyBugClub_WebApp.Controllers
+{
+    public class LoginSignUp : Controller
+    {
+        public IActionResult VerifyAccount()
+        {
+            return View();
+        }
+    }
+}
